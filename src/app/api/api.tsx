385f25c31240d433ade2972252e0683308019630
Peterson Rodrigues <petersonpepi16@gmail.com>
2023-10-19 08:54:35 -0300

    Update api.tsx

--- src/app/api/api.tsx
+++ src/app/api/api.tsx
@@ -2,5 +2,5 @@ import react from "react";
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://chat-tempo-real.onrender.com",
 })
